c87c0d17f4f28dfb808669a52066a18c6ee66a99
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2023-02-08 04:23:03 +0300

    bot

--- function.py
+++ function.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import datetime
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'Серега еблан')
+    await update.message.reply_text(f'Привет!')
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f' {datetime.datetime.now().time()}')
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
