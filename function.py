536069276dea2724bd5bcc83a43ba02d0997185b
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2023-02-09 23:28:03 +0300

    upgrade_dot

--- function.py
+++ function.py
@@ -1,10 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import datetime
+bot = ApplicationBuilder().token("6152526442:AAFiHv3KmgbnkVhLxz9kXNh5GJzqfTqTMSQ").build()
+
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Привет!')
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f' {datetime.datetime.now().time()}')
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'/hi\n/time\n/help\n')
+    await update.message.reply_text(f'/hi\n/time\n/help\n/music_list\n/serega\n')
 
+async def music_list_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'/Travis_Scott__TakeWhatYouWant\n/Travis_Scott__SICKO_MODE\n')
+async def travis_scott__takewhatyouwant_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('music/Post Malone, Ozzy Osbourne, Travis_Scott- Take What You Want.mp3', 'rb'))
+
+async def travis_scott__sicko_mode_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('music/Travis Scott - SICKO MODE.mp3', 'rb'))
+async def buetyful_men(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('photo/sergey.jpg', 'rb'))
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from function import *
 
-
-app = ApplicationBuilder().token("6152526442:AAFiHv3KmgbnkVhLxz9kXNh5GJzqfTqTMSQ").build()
 print('server start')
-app.add_handler(CommandHandler("hi", hi_command))
-app.add_handler(CommandHandler("time", time_command))
-app.add_handler(CommandHandler("help", help_command))
-app.run_polling()
+bot.add_handler(CommandHandler("hi", hi_command))
+bot.add_handler(CommandHandler("time", time_command))
+bot.add_handler(CommandHandler("help", help_command))
+bot.add_handler(CommandHandler("music_list", music_list_command))
+bot.add_handler(CommandHandler("Travis_Scott__TakeWhatYouWant", travis_scott__takewhatyouwant_command))
+bot.add_handler(CommandHandler("Travis_Scott__SICKO_MODE", travis_scott__sicko_mode_command))
+bot.add_handler(CommandHandler("serega", buetyful_men))
+bot.run_polling()
